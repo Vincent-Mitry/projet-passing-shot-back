ff226028d6d6ea72b86a8d3ebcb2eee4507f142b
JulienDabs <j.dabadie81@gmail.com>
2022-06-09 11:32:46 +0200

    julien-build/create-entity-blockedcourt

---
+++ src/Entity/BlockedCourt.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlockedCourtRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BlockedCourtRepository::class)
+ */
+class BlockedCourt
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $startDatetime;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $endDatetime;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $blockedReason;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     */
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartDatetime(): ?\DateTimeInterface
+    {
+        return $this->startDatetime;
+    }
+
+    public function setStartDatetime(\DateTimeInterface $startDatetime): self
+    {
+        $this->startDatetime = $startDatetime;
+
+        return $this;
+    }
+
+    public function getEndDatetime(): ?\DateTimeInterface
+    {
+        return $this->endDatetime;
+    }
+
+    public function setEndDatetime(\DateTimeInterface $endDatetime): self
+    {
+        $this->endDatetime = $endDatetime;
+
+        return $this;
+    }
+
+    public function getBlockedReason(): ?string
+    {
+        return $this->blockedReason;
+    }
+
+    public function setBlockedReason(string $blockedReason): self
+    {
+        $this->blockedReason = $blockedReason;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
