42b1f0e27a53c2b4a629a9e992e5bea215b4cf14
YannisThalgott <yannis.thalgott49@gmail.com>
2022-06-09 17:21:07 +0200

    yannis-feat/wip-user-dynamic

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/utilisateurs", name="app_user", methods={"GET"})
      */
-    public function list(): Response
+    public function list(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
-            'UserController' => 'UserController',
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
