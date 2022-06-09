b6806f1f8edd81615f31e399d414bfcd0ddec575
JulienDabs <j.dabadie81@gmail.com>
2022-06-09 20:23:09 +0200

    julien.fix/fixing-securityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="login", methods={"GET"})
-     */
-    public function login(): Response
-    {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
-    }
+
 }
