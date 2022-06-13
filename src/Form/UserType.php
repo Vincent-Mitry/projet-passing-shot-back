2f7b39e535d2fb7e596b2d6226669cb8166ab934
YannisThalgott <yannis.thalgott49@gmail.com>
2022-06-13 10:57:38 +0200

    yannis-feat/user-crud

---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname')
+            ->add('firstname')
+            ->add('email')
+            ->add('gender')
+            ->add('level')
+            ->add('phone')
+            ->add('password')
+            ->add('picture')
+            ->add('roles')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('birthdate')
+            ->add('club')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
