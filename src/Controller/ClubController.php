e7c035a64d17837ded1da3c379d3e04a0e106fe3
YannisThalgott <yannis.thalgott49@gmail.com>
2022-06-13 11:09:10 +0200

    yannis-feat/club-crud

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -2,20 +2,89 @@
 
 namespace App\Controller;
 
+use App\Entity\Club;
+use App\Form\ClubType;
 use App\Repository\ClubRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/club")
+ */
 class ClubController extends AbstractController
 {
     /**
-     * @Route("/clubs", name="app_club", methods={"GET"})
+     * @Route("", name="app_club", methods={"GET"})
      */
     public function list(ClubRepository $clubRepository): Response
     {
-        return $this->render('club1/index.html.twig', [
+        return $this->render('club/index.html.twig', [
             'clubs' => $clubRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="app_club_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, ClubRepository $clubRepository): Response
+    {
+        $club = new Club();
+        $form = $this->createForm(ClubType::class, $club);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $clubRepository->add($club, true);
+
+            return $this->redirectToRoute('app_club_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('club/new.html.twig', [
+            'club' => $club,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_club_show", methods={"GET"})
+     */
+    public function show(Club $club): Response
+    {
+        return $this->render('club/show.html.twig', [
+            'club' => $club,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_club_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Club $club, ClubRepository $clubRepository): Response
+    {
+        $form = $this->createForm(ClubType::class, $club);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $clubRepository->add($club, true);
+
+            return $this->redirectToRoute('app_club_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('club/edit.html.twig', [
+            'club' => $club,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_club_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Club $club, ClubRepository $clubRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$club->getId(), $request->request->get('_token'))) {
+            $clubRepository->remove($club, true);
+        }
+
+        return $this->redirectToRoute('app_club_index', [], Response::HTTP_SEE_OTHER);
     }
 }
