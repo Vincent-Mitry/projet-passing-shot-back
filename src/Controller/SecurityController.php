f576fc6aad9d96bffe3bdf6160d0c45c982c1b79
JulienDabs <j.dabadie81@gmail.com>
2022-06-17 14:35:43 +0200

    julien-fix/api-public-access+logout-back

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,7 +27,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="logout", methods={"GET"})
+     * @Route("/deconnexion", name="logout", methods={"GET"})
      */
     public function logout(): void
     {
