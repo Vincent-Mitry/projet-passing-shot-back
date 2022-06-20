392e5059baf001fcd7c55761df77eeb27eaada47
Vincent Mitry <vinz.mitry@gmail.com>
2022-06-20 13:19:57 +0200

    vincent-feat/Crud-ajout + liste de terrains bloqués + contraintes de validation

---
+++ migrations/Version20220617162625.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220617162625 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE blocked_court CHANGE start_datetime start_datetime DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE end_datetime end_datetime DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE blocked_court CHANGE start_datetime start_datetime TIME NOT NULL, CHANGE end_datetime end_datetime TIME NOT NULL');
+    }
+}
