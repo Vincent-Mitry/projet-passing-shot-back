1ee62539067f4e84e0e4ab347ae31fe4cb398cac
Vincent Mitry <vinz.mitry@gmail.com>
2022-06-28 15:35:23 +0200

    vincent-julien/minor-fixes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,7 +7,6 @@ use App\Repository\ClubRepository;
 use App\Repository\UserRepository;
 use App\Repository\CourtRepository;
 use App\Repository\ReservationRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,28 +16,19 @@ class MainController extends AbstractController
     /**
      * @Route("", name="app_home", methods={"GET"})
      */
-    public function home(ClubRepository $clubRepository,
-    ReservationRepository $reservationRepository,
-    CourtRepository $courtRepository,
-    UserRepository $userRepository,
-    Timeslots $timeslots,
-    Request $request): Response
-    {
-      
-        $test = $timeslots->getAllAvailabletimeslots(date('y/m/d'));
-
-     
-        
-
+    public function home(
+        ClubRepository $clubRepository,
+        ReservationRepository $reservationRepository,
+        CourtRepository $courtRepository,
+        UserRepository $userRepository,
+        Timeslots $timeslots,
+    ): Response {
         return $this->render('main/index.html.twig', [
             'clubs' => $clubRepository->findLastThree(),
             'reservations' => $reservationRepository->findLastThree(),
             'courts' => $courtRepository->findLastThree(),
             'users' => $userRepository->findLastThree(),
-            'timeslots' => $timeslots->getAllAvailabletimeslots(date('y/m/d')),         
-           
-            
-
+            'timeslots' => $timeslots->getAllAvailabletimeslots(date('Y-m-d')),
         ]);
     }
 }
