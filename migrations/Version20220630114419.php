3dd914174793be6ed2b8dc35cc22b935fe06ebef
YannisThalgott <yannis.thalgott49@gmail.com>
2022-06-30 13:48:52 +0200

    yannis-fix/onDelete-cascade-blockedCourt

---
+++ migrations/Version20220630114419.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220630114419 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE blocked_court DROP FOREIGN KEY blocked_court_ibfk_2');
+        $this->addSql('ALTER TABLE blocked_court ADD CONSTRAINT FK_D4EF58C3E3184009 FOREIGN KEY (court_id) REFERENCES court (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE blocked_court DROP FOREIGN KEY FK_D4EF58C3E3184009');
+        $this->addSql('ALTER TABLE blocked_court ADD CONSTRAINT blocked_court_ibfk_2 FOREIGN KEY (court_id) REFERENCES court (id)');
+    }
+}
