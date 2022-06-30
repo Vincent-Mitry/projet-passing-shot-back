190113a9767d9309d2b1e8820b896b167010eb02
Vincent Mitry <vinz.mitry@gmail.com>
2022-06-30 12:47:08 +0200

    vincent-feat/added-form-configuration-for-genre + added-acl-genres

--- src/Controller/GenderController.php
+++ src/Controller/GenderController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/gender")
+ * @Route("/genres")
  */
 class GenderController extends AbstractController
 {
